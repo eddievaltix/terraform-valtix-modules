36fbc81ad1d1b8cf16f036b751a3f0424397c247
Eddie Tang <eddie@valtix.com>
2020-10-04 22:47:55 +0000

    more outputs needed for the gateway from azure_vnet

--- valtix_azure_vnet/main.tf
+++ valtix_azure_vnet/main.tf
@@ -13,3 +13,39 @@ resource "azurerm_virtual_network" gw_vnet {
     address_prefix    = cidrsubnet(var.vnet_cidr,8,1)
   }
 }
+
+resource "azurerm_network_security_group" gw_mgmt_nsg {
+  name                = "valtix-mgmt-sg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_security_group" gw_dp_nsg {
+  name                = "valtix-datapath-sg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "allow-all"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
--- valtix_azure_vnet/outputs.tf
+++ valtix_azure_vnet/outputs.tf
@@ -2,6 +2,22 @@ output "vnet_id" {
   value = azurerm_virtual_network.gw_vnet.id
 }
 
+output "vnet_name" {
+  value = azurerm_virtual_network.gw_vnet.name
+}
+
+output "vnet_location" {
+  value = azurerm_virtual_network.gw_vnet.location
+}
+
 output "vnet_resource_group" {
   value = azurerm_virtual_network.gw_vnet.resource_group_name
+}
+
+output "nsg_mgmt_id" {
+  value = azurerm_network_security_group.gw_mgmt_nsg.id
+}
+
+output "nsg_datapath_id" {
+  value = azurerm_network_security_group.gw_dp_nsg.id
 }
