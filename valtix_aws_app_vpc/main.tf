23c45ddd2dc37fd70cb0243d58ee9de86e19264d
Eddie Tang <eddie@valtix.com>
2020-10-17 04:39:10 +0000

    changed to ubuntu image to app_vpc

--- valtix_aws_app_vpc/main.tf
+++ valtix_aws_app_vpc/main.tf
@@ -3,10 +3,14 @@
 ######################################
 data "aws_availability_zones" "available" {}
 
-data "aws_ami" "it" {	
-  most_recent = true	
-  owners      = ["902505820678"]	
-  name_regex  = "^valtix-sandbox-server"	
+data "aws_ami" "it" {
+  most_recent = true
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
 }
 
 resource "aws_key_pair" "it" {
@@ -235,19 +239,19 @@ resource "aws_instance" "it_pub" {
 
   provisioner "file" {
     source        = format("${path.cwd}/%s", var.prv_key_file_path)
-    destination   = "/home/centos/.ssh/id_rsa"
+    destination   = "/home/ubuntu/.ssh/id_rsa"
     connection {
       type        = "ssh"
-      user        = "centos"
+      user        = "ubuntu"
       private_key = file(format("${path.cwd}/%s", var.prv_key_file_path))
       host        = aws_instance.it_pub.public_ip
     }
   }
   provisioner "remote-exec" {
-    inline = ["chmod 400 /home/centos/.ssh/id_rsa"]
+    inline = ["chmod 400 /home/ubuntu/.ssh/id_rsa"]
     connection {
       type        = "ssh"
-      user        = "centos"
+      user        = "ubuntu"
       private_key = file(format("${path.cwd}/%s", var.prv_key_file_path))
       host        = aws_instance.it_pub.public_ip
     }
