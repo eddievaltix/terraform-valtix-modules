cdc83292aa8f09c8f1db7f32ea53f84fac64fcfc
Eddie Tang <eddie@valtix.com>
2021-01-27 08:43:43 +0000

    added time delay

--- valtix_aws_transit_gateway_resource_share/main.tf
+++ valtix_aws_transit_gateway_resource_share/main.tf
@@ -11,6 +11,17 @@ data "aws_ram_resource_share" "this" {
   resource_owner = "SELF"
 }
 
+# AWS resources shared via Resource Access Manager can take a few seconds to
+# propagate across AWS accounts after RAM returns a successful association.
+resource "time_sleep" "ram_resource_propagation" {
+  create_duration = "60s"
+
+  triggers = {
+    # This sets up a proper dependency on the RAM association
+    tgw_resource_arn = aws_ram_principal_association.example.resource_arn
+  }
+}
+
 #create a new principal association to the existing resource share
 resource "aws_ram_principal_association" "this" {
   principal          = var.principal
@@ -20,6 +31,5 @@ resource "aws_ram_principal_association" "this" {
 #accept the shared resource on the customer spoke VPC account
 resource "aws_ram_resource_share_accepter" "this" {
   provider  = aws.protected
-  share_arn = aws_ram_principal_association.this.resource_share_arn
-  depends_on = [aws_ram_principal_association.this]
+  share_arn = [time_sleep.ram_resource_propagation.triggers["tgw_resource_arn"]]
 }
